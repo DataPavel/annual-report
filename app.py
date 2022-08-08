7117ec63fb3b25498492f4b925362b963c0df3ff
Pavel Averin <averinjr@gmail.com>
2022-08-08 23:07:21 +0300

    Working on dependent dropdowns

--- app.py
+++ app.py
@@ -1,44 +1,22 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, jsonify
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from wtforms import FileField, SubmitField, DateField, SelectMultipleField
 from werkzeug.utils import secure_filename
 import os
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, DataRequired
 
 import pandas as pd
-import boto3
 
-from dotenv import load_dotenv
-
-def configure():
-    load_dotenv()
-
-configure()
+import utils
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'uploads'
-ALLOWED_EXTENSIONS = ['csv', 'xlsx', 'xls']
 
-# Create the low level functional client
 
-client = boto3.client(
-    's3',
-    aws_access_key_id = os.getenv('aws_access_key_id'), 
-    aws_secret_access_key = os.getenv('aws_secret_access_key'),
-    region_name = os.getenv('region_name')
-)
-bucket = os.getenv('bucket')
 class UploadFile(FlaskForm):
     upload = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-columns = ['Date', 'Company', 'Studio', 'Project', 'Category', 'Country',
-       'Country_code', 'OS', 'Counterparty', 'Amount_USD']
 
 @app.route('/')
 def index():
@@ -50,15 +28,15 @@ def upload():
     if form.validate_on_submit():
         # Save file to the upload folder
         file = form.upload.data
-        if allowed_file(file.filename):
+        if utils.allowed_file(file.filename):
             file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                 app.config['UPLOAD_FOLDER'],
                 secure_filename(file.filename))
             file.save(file_path)
-            if (len(pd.read_csv(file_path).columns) == len(columns))\
-            and (pd.read_csv(file_path).columns == columns).mean():
+            if (len(pd.read_csv(file_path).columns) == len(utils.columns))\
+            and (pd.read_csv(file_path).columns == utils.columns).mean():
                 #Upload file to S3
-                client.upload_file(file_path, bucket, file.filename)
+                utils.client.upload_file(file_path, utils.bucket, file.filename)
                 # Delete file from upload folder
                 os.remove(file_path)
                 flash('The file has been uploaded')
@@ -73,3 +51,44 @@ def upload():
             flash('Make sure you upload file in csv or xlsx format')
             return render_template('upload.html', form=form)
     return render_template('upload.html', form=form)
+
+class FilterForm(FlaskForm):
+
+    start_date = DateField('Start Date', validators=[DataRequired()])
+    end_date = DateField('End Date', validators=[DataRequired()])
+    company_name = SelectMultipleField('Company Name',
+        validate_choice=False, validators=[DataRequired()])
+    studio_name = SelectMultipleField('Studio', choices=[],
+        validate_choice=False, validators=[DataRequired()])
+    product_name = SelectMultipleField('Product', choices=[],
+        validate_choice=False, validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+
+@app.route('/profit/')
+def profit():
+    form = FilterForm()
+    form.company_name.choices=['All'] + utils.unique_value('Company')
+    form.studio_name.choices=['All']+ utils.unique_value('Studio')
+    form.product_name.choices=['All']+ utils.unique_value('Project')
+    return render_template('filter.html', form=form)
+
+
+### TODO
+# https://www.youtube.com/watch?v=I2dJuNwlIH0
+# Dynamic Select Fields
+@app.route('/studio/<company>')
+def studio(company):
+    df = utils.read_file_s3()
+    studios = list(df[df['Company']==company]['Studio'].unique())
+    studioArray = list()
+    for studio in studios:
+        studioObj = {}
+        studioObj['id'] = studio
+        studioObj['name'] = studio
+        studioArray.append(studioObj)
+    return jsonify( {'studios': studioArray})
+
+
+
---
+++ utils.py
@@ -0,0 +1,58 @@
+import boto3
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+# Config-----------------------------------------------------------------
+
+
+def configure():
+    load_dotenv()
+
+configure()
+
+extentions = ['csv', 'xlsx', 'xls']
+
+
+client = boto3.client(
+    's3',
+    aws_access_key_id = os.getenv('aws_access_key_id'), 
+    aws_secret_access_key = os.getenv('aws_secret_access_key'),
+    region_name = os.getenv('region_name')
+)
+bucket = os.getenv('bucket')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in extentions
+
+
+columns = ['Date', 'Company', 'Studio', 'Project', 'Category', 'Country',
+       'Country_code', 'OS', 'Counterparty', 'Amount_USD']
+
+def read_file_s3():
+	keys_list = list()
+	for i in client.list_objects(Bucket=bucket)['Contents']:
+	    keys_list.append(i['Key'])
+	# Create the S3 object
+	obj_list = list()
+	for key in keys_list:
+	    obj = client.get_object(
+	        Bucket = bucket,
+	        Key = key)
+	    obj_list.append(obj)
+	# Read data from the S3 object
+	data_list = list()
+	for obj in obj_list:
+	    data = pd.read_csv(obj['Body'])
+	    data_list.append(data)
+
+	df = pd.concat(data_list, axis=0)
+
+	return df
+
+def unique_value(column_name):
+	df = read_file_s3()
+	col_value_unique = list(df[column_name].unique())
+	return col_value_unique
